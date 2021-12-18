ec221c8f878c455faf52b3c0d97e52938a70c602
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-12-18 17:53:58 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'ready',
-  execute(client) {
+  async execute(client) {
     console.log('Ticket Bot ready!')
     console.log('Thank you very much for using Ticket Bot! Developed with the ❤️ by Sayrix');
     const oniChan = client.channels.cache.get(client.config.ticketChannel)
@@ -26,8 +26,62 @@ module.exports = {
       })
     }
 
-    oniChan.bulkDelete(100).then(() => {
-      sendTicketMSG()
+    const toDelete = 10000;
+
+    async function fetchMore(channel, limit) {
+      if (!channel) {
+        throw new Error(`Expected channel, got ${typeof channel}.`);
+      }
+      if (limit <= 100) {
+        return channel.messages.fetch({
+          limit
+        });
+      }
+
+      let collection = [];
+      let lastId = null;
+      let options = {};
+      let remaining = limit;
+
+      while (remaining > 0) {
+        options.limit = remaining > 100 ? 100 : remaining;
+        remaining = remaining > 100 ? remaining - 100 : 0;
+
+        if (lastId) {
+          options.before = lastId;
+        }
+
+        let messages = await channel.messages.fetch(options);
+
+        if (!messages.last()) {
+          break;
+        }
+
+        collection = collection.concat(messages);
+        lastId = messages.last().id;
+      }
+      collection.remaining = remaining;
+
+      return collection;
+    }
+
+    const list = await fetchMore(oniChan, toDelete);
+
+    let i = 1;
+
+    list.forEach(underList => {
+      underList.forEach(msg => {
+        i++;
+        if (i < toDelete) {
+          setTimeout(function () {
+            msg.delete()
+          }, 1000 * i)
+        }
+      })
     })
+
+    setTimeout(() => {
+      sendTicketMSG()
+    }, i);
   },
 };
