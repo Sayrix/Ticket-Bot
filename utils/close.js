d57b6a8612dff0395527520d6f52940aaf41ab83
Musiker15 <info@musiker15.de>
2023-01-04 22:39:06 +0100

    Move closed ticket to another category

--- utils/close.js
+++ utils/close.js
@@ -72,7 +72,7 @@ module.exports = {
     }).catch(e => console.log(e));
 
     async function close(res) {
-      interaction.channel.setParent(client.config.closeTicketCategoryId);
+      if (client.config.closeTicketCategoryId) interaction.channel.setParent(client.config.closeTicketCategoryId);
       
       interaction.channel.send({
         content: client.locales.ticketTranscriptCreated.replace('TRANSCRIPTURL', `<https://transcript.cf/${res.data.id}>`),
