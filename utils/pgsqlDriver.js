cfc446567f5b2ce7444d970b0dc4fa66fbc863c8
Sayrix <43046854+Sayrix@users.noreply.github.com>
2023-06-18 22:00:17 +0200

    fixed eslint warnings

--- utils/pgsqlDriver.js
+++ utils/pgsqlDriver.js
@@ -6,6 +6,7 @@
  * LICENSE: https://github.com/plexidev/quick.db/blob/dev/LICENSE.md
  */
 
+// eslint-disable-next-line node/no-missing-require
 const { Client } = require("pg");
 
 module.exports = class PostgresDriver {
@@ -32,7 +33,7 @@ module.exports = class PostgresDriver {
 		await this.conn.end();
 	}
 
-	checkConnection(){
+	checkConnection() {
 		if (!this.conn) {
 			throw new Error("No connection to postgres database");
 		}
@@ -40,9 +41,7 @@ module.exports = class PostgresDriver {
 
 	async prepare(table) {
 		this.checkConnection();
-		await this.conn.query(
-			`CREATE TABLE IF NOT EXISTS ${table} (id VARCHAR(255), value TEXT)`
-		);
+		await this.conn.query(`CREATE TABLE IF NOT EXISTS ${table} (id VARCHAR(255), value TEXT)`);
 	}
 
 	async getAllRows(table) {
@@ -50,44 +49,27 @@ module.exports = class PostgresDriver {
 		const queryResult = await this.conn.query(`SELECT * FROM ${table}`);
 		return queryResult.rows.map((row) => ({
 			id: row.id,
-			value: JSON.parse(row.value),
+			value: JSON.parse(row.value)
 		}));
 	}
 
-	async getRowByKey(
-		table,
-		key
-	) {
+	async getRowByKey(table, key) {
 		this.checkConnection();
-		const queryResult = await this.conn.query(
-			`SELECT value FROM ${table} WHERE id = $1`,
-			[key]
-		);
+		const queryResult = await this.conn.query(`SELECT value FROM ${table} WHERE id = $1`, [key]);
 
 		if (queryResult.rowCount < 1) return [null, false];
 		return [JSON.parse(queryResult.rows[0].value), true];
 	}
 
-	async setRowByKey(
-		table,
-		key,
-		value,
-		update
-	) {
+	async setRowByKey(table, key, value, update) {
 		this.checkConnection();
 
 		const stringifiedValue = JSON.stringify(value);
 
 		if (update) {
-			await this.conn.query(
-				`UPDATE ${table} SET value = $1 WHERE id = $2`,
-				[stringifiedValue, key]
-			);
+			await this.conn.query(`UPDATE ${table} SET value = $1 WHERE id = $2`, [stringifiedValue, key]);
 		} else {
-			await this.conn.query(
-				`INSERT INTO ${table} (id, value) VALUES ($1, $2)`,
-				[key, stringifiedValue]
-			);
+			await this.conn.query(`INSERT INTO ${table} (id, value) VALUES ($1, $2)`, [key, stringifiedValue]);
 		}
 
 		return value;
@@ -101,10 +83,7 @@ module.exports = class PostgresDriver {
 
 	async deleteRowByKey(table, key) {
 		this.checkConnection();
-		const queryResult = await this.conn.query(
-			`DELETE FROM ${table} WHERE id = $1`,
-			[key]
-		);
+		const queryResult = await this.conn.query(`DELETE FROM ${table} WHERE id = $1`, [key]);
 		return queryResult.rowCount;
 	}
 };
