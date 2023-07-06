636ef20b317a9e813e95b917a1cc002fe4b66b5d
zhiyan114 <zhiyan114@zhiyan114.com>
2023-07-06 13:55:06 -0400

    Fixed the order which the file is being moved

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-npx prisma db push --schema=./prisma/docker.prisma
-npm run start
 
 # Move config file is found during build process
 if [ -f "./temp_config.jsonc" ]; then
     mv ./temp_config.jsonc ./config/config.jsonc
 fi
+
+npx prisma db push --schema=./prisma/docker.prisma
+npm run start
