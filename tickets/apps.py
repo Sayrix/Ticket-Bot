4b05618ee570e9f54ee107a939896d99dc1093a6
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-13 16:45:44 -0300

    deleting/recreating app

--- tickets/apps.py
+++ tickets/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoredjangoConfig(AppConfig):
+class TicketsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coredjango'
+    name = 'tickets'
