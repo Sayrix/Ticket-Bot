b86ddecf5b9c7465c611b448689b0eb5bf583b5b
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-13 17:10:30 -0300

    Changed comments from Portuguese to English

--- script.py
+++ script.py
@@ -1,24 +1,21 @@
 import os
 import django
 
-# Configurar as definições do Django
+# Setting Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketbotdjango.settings')
 django.setup()
 
 from tickets.models import Tickets, Categorys
 
-
-# Recuperar o campo JSON do banco de dados
+# Retrieve JSON field from database
 ticket = Tickets.objects.all()
 
+# Loop to insert the obtained data into the new table
 for t in ticket:
     Category = Categorys()
     Category.category_ticket = t
     Category.codeName = t.category['codeName']
     Category.name = t.category['name']
-    print(t)
-    print(t.category['codeName'])
-    print(t.category['name'])
     Category.save()
 
 
