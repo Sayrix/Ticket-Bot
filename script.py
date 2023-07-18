66a4257f21b6d892012a94e90d9f45b216c2bcf5
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-18 18:24:41 -0300

    update script to convert new fields with request

--- script.py
+++ script.py
@@ -1,21 +1,40 @@
 import os
 import django
+from discord_requests import discord_user_request
 
 # Setting Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketbotdjango.settings')
 django.setup()
 
-from tickets.models import Tickets, Categorys
+from tickets.models import Tickets, Tickets_Info
 
 # Retrieve JSON field from database
 ticket = Tickets.objects.all()
 
 # Loop to insert the obtained data into the new table
 for t in ticket:
-    Category = Categorys()
-    Category.category_ticket = t
-    Category.codeName = t.category['codeName']
-    Category.name = t.category['name']
-    Category.save()
-
-
+    try:
+        Ticket_InfoCategory = Tickets_Info()
+        Ticket_InfoCategory.category_ticket = t
+        Ticket_InfoCategory.category_codeName = t.category['codeName']
+        Ticket_InfoCategory.category_name = t.category['name']
+        
+        if t.creator:
+            Ticket_InfoCategory.creator_username = discord_user_request(t.creator, 'username')
+            Ticket_InfoCategory.creator_displayname = discord_user_request(t.creator, 'display_name')
+        if t.claimedby:
+            Ticket_InfoCategory.claimedby_username = discord_user_request(t.claimedby, 'username')
+            Ticket_InfoCategory.claimedby_displayname = discord_user_request(t.claimedby, 'display_name')
+            
+        if t.closedby:
+            Ticket_InfoCategory.closedby_username = discord_user_request(t.closedby, 'username')
+            Ticket_InfoCategory.closedby_displayname = discord_user_request(t.closedby, 'display_name')
+        
+        Ticket_InfoCategory.save()
+        print('')
+        print(f'{Ticket_InfoCategory} saved')
+    except:
+        print('')
+        print(f'Something went wrong when trying to save {Ticket_InfoCategory}')
+        
+        
