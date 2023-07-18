affaec0ff14ca414668d410e282744c82b55ad8f
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-18 18:23:45 -0300

    updated admin with new fields

--- tickets/admin.py
+++ tickets/admin.py
@@ -4,13 +4,19 @@ from .models import Tickets, Tickets_Info
 class TicketsAdmin(admin.ModelAdmin):
     list_display = ('channelid', 'messageid', 'reason', 'creator', 'claimedby', 'claimedat', 'closedby', 'closedat', 'closereason', 'transcript')
 
-
 class Tickets_InfoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'codeName', 'name', )
-    list_filter = ('codeName', )
-    list_display_links = ('id', 'codeName', 'name', )
-    search_fields = ('id', 'codeName', 'name', )
-
+    list_display = ('id', 'category_codeName',
+                    'category_name', 'creator_username', 
+                    'create_displayname', 'claimedby_username', 
+                    'claimedby_displayname', 'closedby_username', 
+                    'closedby_displayname', )
+    list_filter = ('category_name', 'category_codeName', )
+    list_display_links = ('id', 'category_codeName', 'category_name', )
+    search_fields = ('id', 'category_codeName',
+                    'category_name', 'creator_username', 
+                    'create_displayname', 'claimedby_username', 
+                    'claimedby_displayname', 'closedby_username', 
+                    'closedby_displayname', )
 
 
 admin.site.register(Tickets_Info, Tickets_InfoAdmin)
