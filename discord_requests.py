9ed56136f14f470e1e5790754b1d1fa2a9149da4
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-19 14:22:23 -0300

    added comments

--- discord_requests.py
+++ discord_requests.py
@@ -1,22 +1,33 @@
 import requests
-import json
+
 
 from dotenv import dotenv_values
 
+# You will need to create an .env and assign the value of your token to the constant TOKEN
 TOKEN = dotenv_values(".env")["TOKEN"]
 
 VERSION = 10
 
-def discord_user_request(userid, campo):    
-    
+# This function needs to receive as a parameter the userid that will be consulted, and which field will be returned
+# If you leave field blank, will retorn the entire JSON
+# The field can be: id, username, avatar, discriminator, public_flags, flags, banner, accent_color
+# global_name, avatar_decoration, display_name, banner_color
+def discord_user_request(userid, field=None):
     r = requests.get(f'https://discord.com/api/v{VERSION}/users/{userid}'
                     , headers={
                         'Authorization': f'Bot {TOKEN}'
                         }
                     )
-    
-    return r.json()[f'{campo}']
+    if field:
+        return r.json()[f'{field}']
+    else:
+        return r.json()
 
+# Just for testing
+    
 if __name__ == '__main__':
     
-    print(discord_user_request('', 'display_name'))
+    # USER just for testing
+    USER = dotenv_values(".env")["USER"]
+    
+    print(discord_user_request(USER))
