380c0734f8499ed8c3d98be0f32ef6a0d6e28060
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-24 11:37:11 -0300

    removed display_names and created tickets_temp_log

--- tickets/models.py
+++ tickets/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Config(models.Model):
-    key = models.CharField(max_length=256, primary_key=True)
+    key = models.CharField(max_length=255, primary_key=True)
     value = models.TextField()
     
     class Meta:
@@ -26,19 +27,24 @@ class Tickets(models.Model):
         managed = False
         db_table = 'tickets'
         
+class Tickets_Temp_Log(models.Model):
+    ticket = models.ForeignKey(Tickets, on_delete=models.CASCADE)
+    action = models.CharField(max_length=50)
+    
+    class Meta:
+        managed = True
+        db_table = 'tickets_temp_log'
+
 class Tickets_Info(models.Model):
     category_ticket = models.OneToOneField(Tickets, on_delete=models.CASCADE)
     category_codeName = models.CharField(max_length=255, null=True)
     category_name = models.CharField(max_length=255, null=True)
     creator_username = models.CharField(max_length=255, null=True)
-    create_displayname = models.CharField(max_length=255, null=True)
     claimedby_username = models.CharField(max_length=255, null=True)
-    claimedby_displayname = models.CharField(max_length=255, null=True)
     closedby_username = models.CharField(max_length=255, null=True)
-    closedby_displayname = models.CharField(max_length=255, null=True)
     
-    def __str__(self):
-        return f'{self.category_ticket}, {self.category_codeName}, {self.category_name}'
+    # def __str__(self):
+    #     return f'{self.category_ticket}, {self.category_codeName}, {self.category_name}'
     
     class Meta:
         managed = True
