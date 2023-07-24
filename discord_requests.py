449000ca1bb6c25049d29f47010e158be884757a
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-24 15:34:42 -0300

    just testingg

--- discord_requests.py
+++ discord_requests.py
@@ -9,9 +9,9 @@ TOKEN = dotenv_values(".env")["TOKEN"]
 VERSION = 10
 
 # This function needs to receive as a parameter the userid that will be consulted, and which field will be returned
-# If you leave field blank, will retorn the entire JSON
+# If you leave field blank, will return the entire JSON
 # The field can be: id, username, avatar, discriminator, public_flags, flags, banner, accent_color
-# global_name, avatar_decoration, display_name, banner_color
+# global_name, avatar_decoration, banner_color
 def discord_user_request(userid, field=None):
     r = requests.get(f'https://discord.com/api/v{VERSION}/users/{userid}'
                     , headers={
@@ -24,10 +24,12 @@ def discord_user_request(userid, field=None):
         return r.json()
 
 # Just for testing
-    
 if __name__ == '__main__':
     
     # USER just for testing
     USER = dotenv_values(".env")["USER"]
     
     print(discord_user_request(USER))
+    
+    
+    
