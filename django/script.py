575084208bc50bc2cc9e76451dbe46a7e80718c5
Rafael Henrique <rafaelh9six@gmail.com>
2023-07-25 14:28:04 -0300

    removed try except

--- django/script.py
+++ django/script.py
@@ -13,28 +13,23 @@ tickets = Tickets.objects.all()
 
 # Loop to insert the obtained data into the new table
 for ticket in tickets:
-    try:
-        ticket_info = Tickets_Info()
-        ticket_info.ticket_info_ticket = ticket
-        ticket_info.category_codeName = ticket.category['codeName']
-        ticket_info.category_name = ticket.category['name']
+    
+    ticket_info = Tickets_Info()
+    ticket_info.ticket_info_ticket = ticket
+    ticket_info.category_codeName = ticket.category['codeName']
+    ticket_info.category_name = ticket.category['name']
+    
+    # Verify if each of the fields isn't null
+    if ticket.creator:
+        ticket_info.creator_username = discord_user_request(ticket.creator, 'username')
         
-        # Verify if each of the fields isn't null
-        if ticket.creator:
-            ticket_info.creator_username = discord_user_request(ticket.creator, 'username')
-            
-        if ticket.claimedby:
-            ticket_info.claimedby_username = discord_user_request(ticket.claimedby, 'username')
-            
-        if ticket.closedby:
-            ticket_info.closedby_username = discord_user_request(ticket.closedby, 'username')
+    if ticket.claimedby:
+        ticket_info.claimedby_username = discord_user_request(ticket.claimedby, 'username')
         
-        
-        ticket_info.save()
-        print('')
-        print(f'Ticket: {ticket_info.pk} saved')
-    except:
-        print('')
-        print(f'Something went wrong when trying to save {ticket_info}')
-        
-        
+    if ticket.closedby:
+        ticket_info.closedby_username = discord_user_request(ticket.closedby, 'username')    
+    
+    ticket_info.save()
+    print('')
+    print(f'Ticket: {ticket_info.pk} saved')
+    
