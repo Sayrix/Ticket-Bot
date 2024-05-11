75fae620bddef6d19c6d32005362503bfddac874
zhiyan114 <zhiyan114@zhiyan114.com>
2024-05-11 13:03:28 -0400

    Fix discordjs latest breaking changes

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,5 @@
 import {BaseCommand, ExtendedClient} from "../structure";
-import {CommandInteraction, SlashCommandBuilder, TextChannel} from "discord.js";
+import {CommandInteraction, SlashCommandBuilder, TextChannel, User} from "discord.js";
 import {log} from "../utils/logs";
 
 /*
@@ -20,7 +20,7 @@ export default class AddCommand extends BaseCommand {
 
 	async execute(interaction: CommandInteraction) {
 
-		const added = interaction.options.getUser("user", true);
+		const added = interaction.options.get("user", true).user as User;
 		const ticket = await this.client.prisma.tickets.findUnique({
 			select: {
 				id: true,
